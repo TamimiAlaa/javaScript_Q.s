f8ab8ebe49a8026a06761b26cf0dcddf48a2b20e
Alaa Tamimi <138234710+TamimiAlaa@users.noreply.github.com>
2025-04-15 12:55:12 +0300

    Add files via upload

---
+++ Introduction.js
@@ -0,0 +1,30 @@
+// Write a function called checkSeason, 
+// it takes a month parameter and returns 
+// the season:Autumn, Winter, Spring or Summer.
+
+// question = document.getElementById("question");
+// ans = document.getElementById("answer");
+
+// //ans.textContent = `Season: ${season}`;
+
+// function checkSeason(season){
+//     if(season == 'december' || season == 'october' )
+//       ans.textContent = 'Season: winter';
+//     //the rest is obvious
+// }
+
+
+
+// question.addEventListener('click',function(){
+//     let season = prompt("Enter a season");
+//     checkSeason(season);
+// });
+
+
+//Math.max returns its largest argument. 
+// Write a function findMax that takes 
+// three arguments and returns their 
+// maximum with out using Math.max method.
+
+
+q = document.getElementById

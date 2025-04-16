53d90fb4a73f44823e8412ddd75535355bf004dc
Alaa Tamimi <Alaa.Ali.Tamimi@gmail.com>
2025-04-16 14:01:09 +0300

    Changed files structure

---
+++ Inprogress q.s/UniqueArray_Q/assets/js/Introduction.js
@@ -0,0 +1,11 @@
+// Write a functions which checks 
+// if all items are unique in the array.
+
+
+
+q = document.getElementById("question");
+ans = document.getElementById("answer");
+ans.textContent = 'Answer';
+q.textContent = 'Enter an array';
+
+
